3cef720c2217b511c7366a10b0e63a16b103c522
phinehasariga <phinehasariga@gmail.com>
2025-04-07 23:30:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (74.30%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        
+        // Step 1: Find the first decreasing element from the end
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If found, find the next larger element to swap
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            // Swap nums[i] and nums[j]
+            swap(nums, i, j);
+        }
+
+        // Step 3: Reverse the subarray from i + 1 to end
+        reverse(nums, i + 1, n - 1);
+    }
+
+    // Swap helper
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // Reverse helper
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    
+    }
+}
