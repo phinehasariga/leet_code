d9cf2f8837814c888af3d9477b6091654cdd321f
phinehasariga <phinehasariga@gmail.com>
2025-07-16 11:02:56 +0530

    Time: 8 ms (75.25%) | Memory: 43.5 MB (91.07%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,80 +1,4 @@
-// class Solution {
-//     public int calculate(String s) {
-//         char[] chs = s.toCharArray();
-//         int curRes = 0,val = 0, res = 0;
-//         char sign = '+';
 
-//         for(int i=0;i<chs.length;i++){
-//             char c = chs[i];
-//             if(Character.isDigit(c))
-//             val = val*10+c-'0';
-
-//             if(c=='+'||c=='*'||c==''|| i==chs.length-1){
-//                 switch(sign){
-//                     case('+'):
-//                     res+=curRes;
-//                     curRes = val;
-//                     break;
-
-//                     case('-'):
-//                     res+=curRes;
-//                     curRes=-val;
-//                     break;
-
-//                     case('*'):
-//                     curRes *= val;
-//                     break;
-
-//                     case('/'):
-//                     curRes /= val;
-//                     break;
-//                 }
-//                 sign = c;
-//                 val = 0;
-//             }
-//         }
-//         return res + curRes;
-//     }
-
-// }
-// class Solution {
-//     public int calculate(String s) {
-//         char[] chs = s.toCharArray();
-//         int curRes = 0, val = 0, res = 0;
-//         char sign = '+';
-
-//         for (int i = 0; i < chs.length; i++) {
-//             char c = chs[i];
-
-//             if (Character.isDigit(c)) {
-//                 val = val * 10 + (c - '0');
-//             }
-
-//             if ((!Character.isDigit(c) && c != '/') || i == chs.length - 1) {
-//                 switch (sign) {
-//                     case '+':
-//                         res += curRes;
-//                         curRes = val;
-//                         break;
-//                     case '-':
-//                         res += curRes;
-//                         curRes = -val;
-//                         break;
-//                     case '*':
-//                         curRes *= val;
-//                         break;
-//                     case '/':
-//                         curRes /= val;
-//                         break;
-//                 }
-//                 sign = c;
-//                 val = 0;
-//             }
-//         }
-
-//         return res + curRes;
-//     }
-// }
 class Solution {
     public int calculate(String s) {
         int res = 0, num = 0, sign = 1;
@@ -120,7 +44,6 @@ class Solution {
                     break;
             }
         }
-
         return res + sign * num;
     }
 }
