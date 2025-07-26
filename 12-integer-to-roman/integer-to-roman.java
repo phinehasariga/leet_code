9d0c78dedf134179afb7b6414873e7396ca301ed
phinehasariga <phinehasariga@gmail.com>
2025-07-26 15:38:48 +0530

    Time: 3 ms (98.15%) | Memory: 44.2 MB (83.21%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,11 +1,11 @@
 class Solution {
     public String intToRoman(int num) {
-                int[] values =    {1000, 900, 500, 400, 100, 90,  50,  40,  10,  9,   5,  4,   1};
+        int[] values =    {1000, 900, 500, 400, 100, 90,  50,  40,  10,  9,   5,  4,   1};
         String[] symbols ={"M", "CM","D", "CD","C", "XC","L", "XL","X", "IX","V", "IV","I"};
 
         StringBuilder roman = new StringBuilder();
 
-        // Step 2: Build the Roman numeral from highest to lowest
+        
         for (int i = 0; i < values.length && num > 0; i++) {
             while (num >= values[i]) {
                 roman.append(symbols[i]);
