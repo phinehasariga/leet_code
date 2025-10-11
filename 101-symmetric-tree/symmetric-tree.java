c1dbd30774a1df3cb7d81963c48ece8cde1c3801
ARIGA PHINEHAS <phinehasariga@gmail.com>
2025-10-11 13:43:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (42.42%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-                if (root == null) {
+        if (root == null) {
             return true;
         }
         return isMirror(root.left, root.right);
@@ -28,6 +28,6 @@ class Solution {
         if (left == null || right == null) {
             return false;
         }
-        return (left.val == right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+        return (left.val == right.val) && isMirror(left.left, right.right) &&  isMirror(left.right, right.left);
     }
 }
