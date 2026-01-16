2061e22efe15c335960df390d771c5f3b1bb64a3
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-01-16 23:23:28 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (54.79%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,11 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n= nums.length;
-        int actualsum = n*(n+1)/2;
-        int resultsum = 0;
-        for(int i=0;i<n;i++){
-            resultsum += nums[i];
+        int n = nums.length;
+        int val1 = 0;
+        int val2 = 0;
+        for(int i=0;i<nums.length;i++){
+            val1 = val1^nums[i];
         }
-        return actualsum - resultsum;
+        for(int i=0;i<=n;i++){
+            val2 = val2^i;
+        }
+        return val1^val2;
     }
 }
