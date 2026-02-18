65054c3a6c34b26d5edc4e84f69c1b8eeff6beb4
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-02-18 11:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (22.64%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int dp = 0,dp1 = 0,dp2 = 0;
+        for(int i=2;i<=n;i++){
+            int onestep = dp1+cost[i-1];
+            int twostep = dp2+cost[i-2];
+            dp = Math.min(onestep,twostep);
+            dp2 = dp1;
+            dp1 = dp;
+        }
+        return dp1;
+    }
+}
