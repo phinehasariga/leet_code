a7a2e3e6944ee43365b674379187ba986382afc3
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-02-25 10:06:19 +0530

    Time: 13 ms (99.08%) | Memory: 51.6 MB (36.58%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -2,14 +2,27 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int contentchildren = 0;
-        int cookieindex = 0;
-        while(cookieindex < s.length && contentchildren < g.length){
-            if(s[cookieindex] >= g[contentchildren]){
-                contentchildren++;
+        // int contentchildren = 0;
+        // int cookieindex = 0;
+        // while(cookieindex < s.length && contentchildren < g.length){
+        //     if(s[cookieindex] >= g[contentchildren]){
+        //         contentchildren++;
+        //     }
+        //     cookieindex++;
+        // }
+        // return contentchildren;
+        int count = 0;
+        int x = s.length-1;
+        int y = g.length-1;
+        while(x>=0 && y>=0){
+            if(s[x] >= g[y]){
+                count++;
+                y--;
+                x--;
+            }else{
+                y--;
             }
-            cookieindex++;
         }
-        return contentchildren;
+        return count;
     }
 }
