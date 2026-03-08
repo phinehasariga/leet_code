ef3c7a11055d2b8f16009531c7b18515d94a6f36
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-03-08 23:48:59 +0530

    Time: 1 ms (99.93%) | Memory: 47.1 MB (26.04%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,9 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
-       for(int i = 0;i < nums.length;i++){
-            result ^= nums[i];
-       } 
-       return result;
+
+        for(int num : nums){
+            result ^= num;
+        }
+
+        return result;
     }
 }
