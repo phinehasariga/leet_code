47dc511512dd405a606dac654d9352f9c442b86a
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-03-12 23:21:06 +0530

    Time: 1 ms (99.81%) | Memory: 94.5 MB (68.14%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyprice = Integer.MAX_VALUE;
-        int maxprofit = 0;
-        for(int i=0;i<prices.length;i++){
+        int buyprice = prices[0];
+        int max = 0;
+        for(int i=1;i<prices.length;i++){
             if(buyprice < prices[i]){
-                int profit = prices[i] - buyprice;
-                maxprofit = Math.max(maxprofit,profit);
+                int profit = prices[i]-buyprice;
+                max = Math.max(max,profit);
             }else{
                 buyprice = prices[i];
             }
         }
-        return maxprofit;
+        return max;
     }
 }
