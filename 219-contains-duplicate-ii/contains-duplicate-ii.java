d473f507b03635fc4d488a7a74a4040078278657
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-05-18 12:16:29 +0530

    Time: 28 ms (16.61%) | Memory: 76 MB (88.20%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+
         for(int i=0;i<nums.length;i++){
-            for(int j= i+1;j<=i+k && j<nums.length;j++){
-                if(nums[i] == nums[j]){
-                    return true;
-                }
+
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+
+            if(set.size() > k){
+                set.remove(nums[i-k]);
             }
         }
         return false;
