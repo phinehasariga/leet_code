8751d34779fa853907e4f16243313edcff5f9a2d
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-09 00:04:53 +0530

    Time: 1 ms (78.66%) | Memory: 46.7 MB (62.63%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,14 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int count = 1;
-        int n = nums.length;
-        for(int i=1;i<n;i++){
-            if(nums[i] != nums[i-1]){
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1] != nums[i]){
                 nums[count] = nums[i];
                 count++;
             }
-        }  
-        return count; 
-
+           
+        }
+         return count;
     }
 }
