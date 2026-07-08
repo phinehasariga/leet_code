b423bf14188ba1f2040c4f64ddfdde93f55b12e4
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-08 22:19:37 +0530

    Time: 5 ms (80.04%) | Memory: 45.5 MB (96.89%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,11 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
+        if(x < 0){
+            return false;
+        }
         int reversed = 0;
         int num = x;
         while(num > 0){
-            reversed = reversed * 10+(num%10);
-            num = num/10;
+            int digit = num % 10;
+            num/=10;
+            reversed = reversed * 10 + digit;
         }
         return x == reversed;
     }
