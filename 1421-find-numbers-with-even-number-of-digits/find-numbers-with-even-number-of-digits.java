fbc20ff4d4576434ecdb832ede4070cfb67ce7c2
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-09 22:27:37 +0530

    Time: 2 ms (33.00%) | Memory: 45.6 MB (7.54%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,27 +1,12 @@
 class Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
-        for (int num : nums) {
-            if (hasEvenDigits(num)) {
+        for(int num : nums){
+            int digits = String.valueOf(num).length();
+            if(digits % 2 == 0){
                 count++;
             }
         }
         return count;
-    }
-    
-    private boolean hasEvenDigits(int num) {
-
-        num = Math.abs(num);
-
-        int digitCount = 0;
-        if (num == 0) {
-            digitCount = 1;
-        } else {
-            while (num > 0) {
-                digitCount++;
-                num /= 10;
-            }
-        }
-        return digitCount % 2 == 0;
     }
 }
