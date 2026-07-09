81b86f7970c82480f7c18e0b6f342992679fad2f
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-09 22:38:34 +0530

    Time: 3 ms (18.97%) | Memory: 47.9 MB (20.21%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums == null || nums.length <= 1){
+        if(nums == null || nums.length == 0){
             return;
         }
         int j = -1;
@@ -13,7 +13,8 @@ class Solution {
         if(j == -1){
             return;
         }
-        for(int i=j+1;i<nums.length;i++){
+
+        for(int i = j+1;i<nums.length;i++){
             if(nums[i] != 0){
                 int temp = nums[i];
                 nums[i] = nums[j];
