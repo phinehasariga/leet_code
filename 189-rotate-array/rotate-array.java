fbea7a6818e5336673dd03c2c0187c9c45f9782d
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-09 22:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 61.3 MB (40.75%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,21 +1,36 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        // int n = nums.length;
+        // k = k%n;
+        // for(int start=0,end=n-1;start < end;start++,end--){
+        //     int temp = nums[start];
+        //     nums[start] = nums[end];
+        //     nums[end] = temp;
+        // }
+        // for(int start = 0,end = k-1;start < end;start++,end--){
+        //     int temp = nums[start];
+        //     nums[start] = nums[end];
+        //     nums[end] = temp;
+        // }
+        // for(int start = k,end = n-1;start < end;start++,end--){
+        //     int temp = nums[start];
+        //     nums[start] = nums[end];
+        //     nums[end] = temp;
+        // }
         int n = nums.length;
-        k = k%n;
-        for(int start=0,end=n-1;start < end;start++,end--){
+        k = k % n;
+        swap(nums, 0, n-1);
+        swap(nums, 0, k-1);
+        swap(nums, k, n-1);
+
+    }
+    public static void swap(int[]nums,int start,int end){
+        while(start < end){
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
-        }
-        for(int start = 0,end = k-1;start < end;start++,end--){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-        }
-        for(int start = k,end = n-1;start < end;start++,end--){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
