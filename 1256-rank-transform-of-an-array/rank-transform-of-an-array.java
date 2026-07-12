40ff0b9ae924f2e01fd4b87964ef8989d234eff3
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-12 23:13:34 +0530

    Time: 30 ms (71.06%) | Memory: 75.2 MB (76.06%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int n = arr.length;
+        int[] c = Arrays.copyOf(arr,n);
+        Arrays.sort(c);
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        int currrank = 1;
+        for(int i : c){
+            if(!map.containsKey(i)){
+                map.put(i,currrank);
+                currrank++;
+            }
+        }
+        int[] res = new int[n];
+        for(int i = 0;i<n;i++){
+            res[i] = map.get(arr[i]);
+        }
+        return res;
+    }
+}
