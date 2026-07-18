4962f2f568fd011edc40e4b298327921cb12127d
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-18 15:55:28 +0530

    Time: 23 ms (93.83%) | Memory: 48.9 MB (46.23%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,15 +1,16 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int ps = 0;
+        int prefixsum = 0;
         int count = 0;
-        HashMap<Integer,Integer> psfrequency = new HashMap<>();
-        psfrequency.put(0,1);
-        for(int num:nums){
-            ps += num;
-            if(psfrequency.containsKey(ps-k)){
-                count += psfrequency.get(ps-k);
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        map.put(0,1);
+        for(int num : nums){
+            prefixsum += num;
+            if(map.containsKey(prefixsum - k)){
+                count += map.get(prefixsum - k);
             }
-            psfrequency.put(ps,psfrequency.getOrDefault(ps,0)+1);
+            map.put(prefixsum,map.getOrDefault(prefixsum,0)+1);
         }
         return count;
     }
