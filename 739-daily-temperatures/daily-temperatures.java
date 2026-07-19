6a86ec1ae5e4eb7c8b05e6b70693311ac2ce22dd
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-19 17:03:40 +0530

    Time: 79 ms (13.47%) | Memory: 106.5 MB (73.48%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -2,28 +2,22 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
         Stack<Integer> s = new Stack<>();
-        // HashMap<Integer,Integer> map = new HashMap<>();
-        ArrayList<Integer> res = new ArrayList<>();
+        
+        int[] ans = new int[n];
 
-        // map.put(temperatures[n-1],0);
-        // s.push(n-1);
         for(int i=n-1;i>=0;i--){
             while(!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]){
                 s.pop();
             }
             if(s.isEmpty()){
-                res.add(0);
+                ans[i] =0;
             }else{
-               res.add(Math.abs(i-s.peek()));
+               ans[i]= Math.abs(i-s.peek());
             }
             s.push(i);
     
         }
-        Collections.reverse(res);
-        int[] ans = new int[n];
-        for(int i=0;i<n;i++){
-            ans[i] = res.get(i);
-        }
+     
         return ans;
     }
 }
