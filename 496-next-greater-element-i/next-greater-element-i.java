5801d28b4561fe91e6d30eff31ebe71c2104c486
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-19 15:25:27 +0530

    Time: 5 ms (16.08%) | Memory: 45.6 MB (11.18%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> s = new Stack<>();
+
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!s.isEmpty() && nums2[s.peek()] <= nums2[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                map.put(nums2[i],-1);
+            }else{
+                map.put(nums2[i],nums2[s.peek()]);
+            }
+            s.push(i);
+        }
+        int[] res = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
+    }
+}
