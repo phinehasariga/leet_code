7ec3deb192cc82c372822ac695e8c1d93f306fbf
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-25 13:27:43 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (92.77%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -1,18 +1,34 @@
 class Solution {
     public int maxProduct(int n) {
-        int p = 0;
-        int maxp = 0;
-        String s = Integer.toString(n);
-        for(int i=0;i<s.length();i++){
-            char d = s.charAt(i);
-            int digit = Character.getNumericValue(d);
-            for(int j=i+1;j<s.length();j++){
-                char d2 = s.charAt(j);
-                int digit2 = Character.getNumericValue(d2);
-                p = digit * digit2;
-                maxp = Math.max(maxp,p);
+        // int p = 0;
+        // int maxp = 0;
+        // String s = Integer.toString(n);
+        // for(int i=0;i<s.length();i++){
+        //     char d = s.charAt(i);
+        //     int digit = Character.getNumericValue(d);
+        //     for(int j=i+1;j<s.length();j++){
+        //         char d2 = s.charAt(j);
+        //         int digit2 = Character.getNumericValue(d2);
+        //         p = digit * digit2;
+        //         maxp = Math.max(maxp,p);
+        //     }
+        // }
+        // return maxp;
+        int max1 = 0, max2 = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit > max1) {
+                max2 = max1;
+                max1 = digit;
+            } else if (digit > max2) {
+                max2 = digit;
             }
+
+            n /= 10;
         }
-        return maxp;
+
+        return max1 * max2;
     }
 }
