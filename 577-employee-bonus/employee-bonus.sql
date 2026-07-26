a720e567de3a8863cf10963cdef5395d9203cbda
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-26 15:05:53 +0530

    Time: 981 ms (69.33%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-select e.name,b.bonus
-from employee as e 
+select name , bonus
+from Employee 
 left join bonus as b
-on e.empid = b.empid
-where b.bonus < 1000 or b.bonus is null
+on Employee.empId = b.empId
+where bonus < 1000 or bonus is null;
