11f357073233a4e2e30398fb2a988c0c033a55f7
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-26 18:19:45 +0530

    Time: 419 ms (82.96%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,10 +1,7 @@
-# Write your MySQL query statement below
-SELECT
-  MAX(t.num) AS num
+SELECT MAX(num) AS num
 FROM (
-  SELECT
-    num
-  FROM MyNumbers
-  GROUP BY num
-  HAVING COUNT(*) = 1
-) AS t;
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS unique_numbers;
