cd209c803522fc028b627955be2170424a0d05c8
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-29 19:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (47.29%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,31 +1,32 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length-1;
-        while(low <= high){
-            int mid = low+(high-low)/2;
+        int l = 0;
+        int r = nums.length - 1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
             if(nums[mid] == target){
                 return true;
             }
-            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
-                low++;
-                high--;
+            if(nums[l] == nums[mid] && nums[mid]==nums[r]){
+                l++;
+                r--;
                 continue;
             }
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid]){
-                    high = mid-1;
+            if(nums[l] <= nums[mid]){
+                if(nums[l] <= target && target <= nums[mid]){
+                    r = mid-1;
                 }else{
-                    low = mid+1;
+                    l = mid+1;
                 }
             }else{
-                if(nums[mid] <= target && target <= nums[high]){
-                    low = mid+1;
+                if(nums[mid] <= target && target <= nums[r]){
+                    l = mid+1;
                 }else{
-                    high = mid-1;
+                    r = mid-1;
                 }
             }
         }
         return false;
+
     }
 }
