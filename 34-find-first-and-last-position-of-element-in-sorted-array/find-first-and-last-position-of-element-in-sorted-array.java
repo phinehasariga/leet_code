69d46fcd8fffec1e1aff6bcbbd5148bf4f3db499
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-29 18:28:41 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (51.92%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,35 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
-        
-        int left = 0, right = nums.length - 1;
-        
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
+        int[] res = {-1,-1};
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] < target){
+                left = mid+1;
+            }else if(nums[mid]  > target){
+                right = mid-1;
+            }else{
+                res[0] = mid;
                 right = mid - 1;
-            } else {
-                result[0] = mid; 
-                right = mid - 1; 
             }
+            
         }
-        
         left = 0;
-        right = nums.length - 1;
-        
-       
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
+        right = nums.length -1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] < target){
+                left  = mid+1;
+            }
+            else if(nums[mid] > target){
                 right = mid - 1;
-            } else {
-                result[1] = mid;
-                left = mid + 1; 
+            }
+            else{
+                res[1] = mid;
+                left = mid+1;
             }
         }
-        
-        return result;
+        return res;
     }
 }
