acdf773727ee754c9f837dc2a66c90bc590e8deb
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-07-29 18:00:14 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (14.05%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,16 +1,16 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int low  = 0;
-        int high = n-1;
-        while(low <= high){
-            int mid = low+(high - low)/2;
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            int mid = left+(right - left)/2;
             if(nums[mid] == target){
                 return mid;
-            }else if(nums[mid] < target){
-                low = mid + 1;
+            }
+            else if(nums[mid] < target){
+                left = mid+1;
             }else{
-                high = mid - 1;
+                right = mid-1;
             }
         }
         return -1;
