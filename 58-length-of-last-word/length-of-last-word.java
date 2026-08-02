4800c59989649405d7aa3a1f4692d630ee3563b7
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-08-02 22:26:04 +0530

    Time: 2 ms (10.56%) | Memory: 43 MB (52.60%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,19 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int length = 0;
-        int i = s.length() - 1;
-        
-        // Skip trailing spaces
-        while (i >= 0 && s.charAt(i) == ' ') {
-            i--;
-        }
-        
-        // Count characters of the last word
-        while (i >= 0 && s.charAt(i) != ' ') {
-            length++;
-            i--;
-        }
-        
-        return length;
+        String[] res = s.trim().split("\\s+");
+        String r = res[res.length-1];
+        int ans = r.length();
+        return ans;
     }
 }
