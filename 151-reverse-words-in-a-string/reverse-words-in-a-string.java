8829724719b8fb0d38f3c14ed57ed288916dea35
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-08-02 22:15:49 +0530

    Time: 11 ms (10.96%) | Memory: 46.5 MB (18.05%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,8 +1,7 @@
 class Solution {
     public String reverseWords(String s) {
         s = s.trim();
-        s = s.replaceAll("\\s+", " ");
-        String[] str = s.split(" ");
+        String[] str = s.split("\\s+");
         String res = "";
         for(int i=str.length-1;i>0;i--){
             res += str[i];
