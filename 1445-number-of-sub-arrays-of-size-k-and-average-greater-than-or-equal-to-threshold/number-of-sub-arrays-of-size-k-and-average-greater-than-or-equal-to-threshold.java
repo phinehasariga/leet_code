e3397cf5f578c191f39afe78271bddb81ebb7ffc
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-08-14 15:24:19 +0530

    Time: 3 ms (86.34%) | Memory: 71.6 MB (82.19%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -6,13 +6,13 @@ class Solution {
         for(int i=0;i<k;i++){
             sum += arr[i];
         }
-        if(sum / k >= threshold){
+        if(sum/k >= threshold){
             count++;
-        }  
+        }
         for(int i=k;i<n;i++){
             sum -= arr[i-k];
             sum += arr[i];
-            if(sum / k >= threshold){
+            if(sum /k >= threshold){
                 count++;
             }
         }
