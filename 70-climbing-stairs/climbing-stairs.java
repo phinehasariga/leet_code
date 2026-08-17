a641807af3b8198a5dd4617a2d790db11a7ad730
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-08-18 00:18:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n == 0 || n == 1){
-            return 1;
+        if(n <= 1){
+            return n;
         }
-        int prev2 = 1;
-        int prev1 = 2;
-        for(int i=3;i<=n;i++){
-            int curr = prev1+prev2;
-            prev2 = prev1;
-            prev1 = curr;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return prev1;
+        return dp[n];
     }
 }
