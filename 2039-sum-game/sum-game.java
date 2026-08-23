11a5de2c526c17509a5f9c993995a76221d48183
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-08-23 23:45:02 +0530

    Time: 7 ms (79.56%) | Memory: 46.9 MB (83.94%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
+            }
+        }
+
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
+            }
+        }
+
+        if (((leftQ + rightQ) & 1) == 1) {
+            return true;
+        }
+
+        int diff = leftSum - rightSum;
+        int qDiff = leftQ - rightQ;
+
+        return diff != -(qDiff / 2) * 9;
+    }
+}
