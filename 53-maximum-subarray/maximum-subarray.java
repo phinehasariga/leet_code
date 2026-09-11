6e66daa39b640087162cc093bdcecfe9b78f65a7
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-09-11 23:46:48 +0530

    Time: 1 ms (99.92%) | Memory: 77.2 MB (66.16%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,8 +3,8 @@ class Solution {
         int currsum = nums[0];
         int maxsum = nums[0];
         for(int i=1;i<nums.length;i++){
-            currsum = Math.max(nums[i] , currsum + nums[i]);
-            maxsum = Math.max(currsum,maxsum);
+            currsum = Math.max(nums[i],currsum+nums[i]);
+            maxsum = Math.max(maxsum,currsum);
         }
         return maxsum;
     }
