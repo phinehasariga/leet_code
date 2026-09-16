eaac332281fe55a57f32be9ee38a14406890cb9b
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-09-16 18:58:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (46.93%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean check(int[] nums) {
-        if(nums==null || nums.length<=1){
+        if(nums == null || nums.length == 0){
             return true;
         }
         int count = 0;
-        for(int i = 0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){
             if(nums[i] > nums[(i+1) % nums.length]){
                 count++;
             }
