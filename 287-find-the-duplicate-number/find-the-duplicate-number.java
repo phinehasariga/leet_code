9302e4c88200a56d8ecffede9b258982f3df3649
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-09-16 19:33:05 +0530

    Time: 2 ms (98.62%) | Memory: 85.6 MB (26.30%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,13 +1,21 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashSet<Integer> s = new HashSet<>();
-        int ans = -1; 
+        // HashSet<Integer> s = new HashSet<>();
+        // int ans = -1; 
+        // for(int i=0;i<nums.length;i++){
+        //     if(s.contains(nums[i])){
+        //         ans = nums[i];
+        //     }
+        //     s.add(nums[i]);
+        // }
+        // return ans;
+        boolean[] arr = new boolean[nums.length];
         for(int i=0;i<nums.length;i++){
-            if(s.contains(nums[i])){
-                ans = nums[i];
+            if(arr[nums[i]] == true){
+                return nums[i];
             }
-            s.add(nums[i]);
-        }
-        return ans;
+            arr[nums[i]] = true;
+        } 
+        return -1;
     }
 }
