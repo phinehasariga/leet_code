581936f4b6d0ed456b11f4afb9298900e7d13ffa
ARIGA PHINEHAS <phinehasariga@gmail.com>
2026-09-20 21:33:26 +0530

    Time: 2 ms (99.31%) | Memory: 44.2 MB (82.72%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -5,7 +5,8 @@ class Solution {
         while(left < right){
             while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
                 left++;
-            }while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
             }
             if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
